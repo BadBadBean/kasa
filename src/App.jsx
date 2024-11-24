4cf26f3f761e263ee31c7cca389955dab0325871
Delphine <mallet.delphine@hotmail.fr>
2024-11-24 01:10:17 +0100

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './style/main.scss'
 import { Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
       <div className="app">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Header />} />
         </Routes>
       </div>  
   )
