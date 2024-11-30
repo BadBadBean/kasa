b6969e93036faab643392a2c2610a7c423883b64
Delphine <mallet.delphine@hotmail.fr>
2024-11-30 20:48:42 +0100

    adding 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/a-propos" element={<About />} /> 
+          <Route path="/a-propos" element={<About />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </main>  
     <Footer />
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function NotFound() {
+    return (
+    <div className="notfound">
+      <h2 className="notfound_title">404</h2>
+      <h3 className="notfound_subtitle">Oups! La page que vous demandez n&apos;existe pas.</h3>
+      <Link to="/" className="notfound_link">Retourner sur la page d’accueil</Link>
+    </div>
+  )
+}
