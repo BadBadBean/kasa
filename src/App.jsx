e6e67bb97744876d023aed84f69e84d1319c57a0
Delphine <mallet.delphine@hotmail.fr>
2024-12-02 15:19:03 +0100

    page 404 responsive

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/a-propos" element={<About />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>  
     <Footer />
